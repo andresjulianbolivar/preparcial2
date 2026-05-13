960ab60d2b49aa04f0092124879b5d293326c134
andres bolivar <a.bolivarc@uniandes.edu.co>
2026-05-13 13:31:43 -0500

    Add: Registro del esquema travelPlan en su respectivo módulo

--- src/travel-plan/travel-plan.module.ts
+++ src/travel-plan/travel-plan.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CountryModule } from 'src/country/country.module';
+import { TravelPlan, TravelPlanSchema } from './travel-plan.schema';
 
 @Module({
-  imports: [CountryModule],
-  controllers: [TravelPlanController],
-  providers: [TravelPlanService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: TravelPlan.name, schema: TravelPlanSchema },
+    ]),
+    CountryModule,
+  ],
 })
 export class TravelPlanModule {}
